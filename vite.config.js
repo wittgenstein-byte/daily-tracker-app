403fd48e33f2f447509735857c051665148eb430
Wittensky <nrvsrpl@gmail.com>
2026-06-07 23:22:31 +0700

    moved panel UI

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,16 @@ export default defineConfig({
   plugins: [aiApiProxy()],
   server: {
     allowedHosts: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   }
 });
